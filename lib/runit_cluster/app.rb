98df26149d0f6d90f0ff9404878e04c64e021a1d
Maxim Filatov <pipopolam@gmail.com>
2011-09-26 23:05:47 +0400

    Stub for main application file

---
+++ lib/runit_cluster/app.rb
@@ -0,0 +1,10 @@
+require 'yajl'
+require 'haml'
+require 'i18n'
+require 'sinatra/base'
+require 'runit_cluster/version'
+
+if RUBY_VERSION >= '1.9'
+  Encoding.default_external = "utf-8"
+  Encoding.default_internal = "utf-8"
+end
