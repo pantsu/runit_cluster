bfa48d728e37721b6decf84bfe813ede910b7d82
Valentin Eremin <valentin.a.eremin@gmail.com>
2011-09-28 00:38:44 +0400

    Rdoc added

--- lib/runit_cluster/config.rb
+++ lib/runit_cluster/config.rb
@@ -1,6 +1,10 @@
 module RunitCluster
   class Config
     class << self
+
+      # Defines default configuration parameters (localhost is default)
+      #
+      # @return [Hash]
       def defaults
         @defaults ||= {
           name: `hostname`.chomp,
@@ -10,19 +14,41 @@ module RunitCluster
           pass: "admin"
         }
         end
+      # Add parameters to configuration
+      #
+      # @return [Hash]
       def use
         @configuration ||= {}
         yield @configuration
       end
+
+      # Collect configuration parameters. If parameter does not exist, defaults will be used.
+      #
+      # @param [Hash]
+      # @return [Hash]
       def setup(settings = {})
         @configuration = defaults.merge(settings)
       end
+
+      # Check for parameter's existance
+      #
+      # @param [key]
+      # @return [Boolean]
       def key?(key)
         @configuration.key?(key)
       end
+
+      # Get the value of the element of configuration Hash
+      #
+      # @param [key, value]
+      # @return [String]
       def [](key)
         (@configuration ||= setup)[key]
       end
+
+      # Set the value of the element of configuration Hash
+      # @param [key, value]
+      # @return [String]
       def []=(key, value)
         (@configuration ||= setup)[key] = value
       end
