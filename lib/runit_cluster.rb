fee4e444abc7d98bcb8d7617acb64fc37c2633ab
Maxim Filatov <pipopolam@gmail.com>
2011-09-30 14:09:12 +0400

    Require RunitCluster::Host in lib/runit_cluster

--- lib/runit_cluster.rb
+++ lib/runit_cluster.rb
@@ -1,6 +1,8 @@
 require "runit_cluster/version"
 require "runit_cluster/app"
 require "runit_cluster/config"
+require "runit_cluster/host"
+
 require "json"
 module RunitCluster
   # Your code goes here...
