f65b36bba0eb3bf024a2efc50a61e40520cf0878
Maxim Filatov <pipopolam@gmail.com>
2011-10-10 10:24:31 +0400

    Default sinatra app

--- lib/runit_cluster/app.rb
+++ lib/runit_cluster/app.rb
@@ -8,3 +8,19 @@ if RUBY_VERSION >= '1.9'
   Encoding.default_external = "utf-8"
   Encoding.default_internal = "utf-8"
 end
+
+class RunitCluster::Application < Sinatra::Base
+
+  CONTENT_TYPES = {
+    :html => 'text/html',
+    :txt  => 'text/plain',
+    :css  => 'text/css',
+    :js   => 'application/x-javascript',
+    :json => 'application/json'
+  }.freeze
+
+  get '/' do
+    haml :index
+  end
+
+end
